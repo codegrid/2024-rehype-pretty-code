1dcd6c39b5f9a5f8ea5bd2838f8589098cf9cdb9
Kanno Ami (tomixy) <tomix05may@gmail.com>
2024-03-18 10:04:50 +0900

    rehype-pretty-codeの設定

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,22 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
+// rehype-pretty-codeからrehypeプラグインをimport
+import rehypePrettyCode from 'rehype-pretty-code'
 
-// https://astro.build/config
-export default defineConfig({});
+/** @type {import('rehype-pretty-code').Options} */
+const prettyCodeOptions = {
+  /** これから追加していく */
+}
+
+export default defineConfig(
+  /** @type {import('astro').AstroUserConfig} */ {
+    markdown: {
+      // Astroビルドインのシンタックスハイライト機能を無効化
+      syntaxHighlight: false,
+      // 代わりにRehype Pretty Codeを使う
+      rehypePlugins: [
+        // [plugin, option] という書式
+        [rehypePrettyCode, prettyCodeOptions],
+      ],
+    },
+  }
+)
