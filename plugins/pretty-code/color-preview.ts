e0dfa32929275023a34de7fa8527bf279637767b
tetracalibers <tomix05may@gmail.com>
2024-07-11 16:29:22 +0900

    color-preview.ts: tokenElementsを走査し、TextWithRangeの配列を作成

--- plugins/pretty-code/color-preview.ts
+++ plugins/pretty-code/color-preview.ts
@@ -1,5 +1,6 @@
 import type { LineElement } from 'rehype-pretty-code'
 import type { Element, Text } from 'hast'
+import { toString } from 'hast-util-to-string'
 
 interface TextWithRange {
   text: string // テキスト
@@ -25,6 +26,24 @@ const createText = (text: string): Text => ({
 export const addColorPreview = (lineElement: LineElement) => {
   // 行を描画する要素の子要素は、各トークンを描画する要素
   const tokenElements = lineElement.children
+
+  // 各トークンの文字列と、その文字列が行全体のどこからどこまでの範囲にあるかを集計
+  const tokens = tokenElements.reduce<TextWithRange[]>(
+    (prevAll, tokenElement) => {
+      // hAST要素からテキストを取得
+      const text = toString(tokenElement)
+      // ここまで集計したトークンのうち、最後のトークンを取得
+      const prev = prevAll[prevAll.length - 1]
+      // まだ集計したトークンがない場合は0から開始
+      // そうでない場合は、前のトークンのendから1文字読み進める
+      const start = prev ? prev.end + 1 : 0
+      // startからtextの文字数分読み進めたものがend
+      const end = start + text.length - 1
+      // 集計済みトークンに追加
+      return [...prevAll, { text, start, end }]
+    },
+    []
+  )
 
   // WIP: 続く…
 }
