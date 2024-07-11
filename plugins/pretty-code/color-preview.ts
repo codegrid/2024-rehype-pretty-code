91885ff30f54b0dec95ff556b1fc1dfa35b6616f
tetracalibers <tomix05may@gmail.com>
2024-07-11 11:53:02 +0900

    color-preview.ts: 色プレビュー要素をhASTオブジェクトとして作成する関数を定義

--- plugins/pretty-code/color-preview.ts
+++ plugins/pretty-code/color-preview.ts
@@ -1,4 +1,15 @@
 import type { LineElement } from 'rehype-pretty-code'
+import type { Element } from 'hast'
+
+const createColorPreviewElement = (color: string): Element => ({
+  type: 'element',
+  tagName: 'span',
+  properties: {
+    'data-color-preview': color,
+    style: `--preview-color: ${color};`,
+  },
+  children: [{ type: 'text', value: '' }], // hastのElement型にはchildrenが必須
+})
 
 export const addColorPreview = (lineElement: LineElement) => {
   // WIP：これから実装する
